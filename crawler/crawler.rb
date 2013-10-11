96cd0c3d8dc087debffe0431ea039d221abb0546
Gormisdomai <gormisdomai@gmail.com>
2013-10-11 14:56:05 +0100

    File now dumps all links to a text file

--- crawler/crawler.rb
+++ crawler/crawler.rb
@@ -7,13 +7,14 @@
 
 require 'net/http'
 
-url = URI.parse('httP://www.wikipedia.org/')
+url = URI.parse('httP://www.reddit.com/r/ruby')
 req = Net::HTTP::Get.new(url.path)
-res = Net::HTTP.start(url.host, url.port)
- 
-{
+res = Net::HTTP.start(url.host, url.port){
 	|http|http.request(req)
 					  
 }
-
-puts res.body
+#puts res.body
+conts = res.body.split('<a href="')
+conts.shift
+f = File.new("reddit.txt", 'w')
+conts.each {|x| f.write(x.split('"')[0])}
